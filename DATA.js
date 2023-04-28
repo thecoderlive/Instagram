91b13ac40cf44c4be933deb41813642b26c88388
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-04-28 15:56:26 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "32d76ce4-3965-4ae2-ae25-6e233ad92a95",
+    "home_feed": [
+        {
+            "id": "588b6b1f-70c1-4272-ae6d-aea8a1fe2875",
+            "artist_name": "join",
+            "home_carousel": [
+                {
+                    "id": "1ad318c5-4cf4-485d-b6b8-d67100f9138f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/248.png"
+                },
+                {
+                    "id": "9d444272-8bcf-48c1-b28d-255305012bb2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/316.png"
+                },
+                {
+                    "id": "9f0bbc80-52d6-4258-b58a-dcec4fd064ec",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/336.png"
+                },
+                {
+                    "id": "43057f59-8a22-4570-9c07-e67dc78ec0c9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/51.png"
+                },
+                {
+                    "id": "ade0c411-51ed-4232-b024-463a8fe01167",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/153.png"
+                }
+            ],
+            "art_caption": "upon",
+            "art_description": "Remember hospital foreign. Study article sound decide sometimes TV."
+        },
+        {
+            "id": "a8ed5299-7419-4355-a64d-9289ff26027b",
+            "artist_name": "power",
+            "home_carousel": [
+                {
+                    "id": "347b041c-2eb1-458c-8091-bc1026c79e2c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/204.png"
+                },
+                {
+                    "id": "a005abfb-4004-4965-8d62-eab95c6ce8e4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/272.png"
+                },
+                {
+                    "id": "b86a427e-cd0e-4af9-9cd8-46bb8c6f8510",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/217.png"
+                },
+                {
+                    "id": "80520327-0b2f-4bf3-8312-5df06c7b359a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/123.png"
+                },
+                {
+                    "id": "cde40559-5421-4336-a0fc-9818c323646e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/215.png"
+                }
+            ],
+            "art_caption": "however",
+            "art_description": "Size figure possible act claim occur. I music him mission. Become value your so listen suggest everyone program."
+        },
+        {
+            "id": "40863800-ca87-4e25-9b68-88268a02d3bb",
+            "artist_name": "list",
+            "home_carousel": [
+                {
+                    "id": "fcdd3b49-cfc3-41a8-87d0-b6582188c95f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/334.png"
+                },
+                {
+                    "id": "e18cc6a3-5116-45d1-9ff7-9cc4a43962ae",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/335.png"
+                },
+                {
+                    "id": "accb87ee-f3cb-4986-af75-89e54ae1b5d1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/307.png"
+                },
+                {
+                    "id": "913ef6c2-f0bb-4850-8cc1-40aefd82eb58",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/59.png"
+                },
+                {
+                    "id": "cba9eeb2-a94f-4acc-a1db-25c68e47f6a7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/234.png"
+                }
+            ],
+            "art_caption": "thought",
+            "art_description": "Lot instead prevent add those above activity voice. Whole always upon building."
+        },
+        {
+            "id": "c586f9be-8eaa-4979-8aed-beff7a21a9e7",
+            "artist_name": "moment",
+            "home_carousel": [
+                {
+                    "id": "a5b50f5d-351d-45c9-a15c-c1f6ddecbdaf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/329.png"
+                },
+                {
+                    "id": "7f3078b9-122a-473b-a536-62b0c6adcb94",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/11.png"
+                },
+                {
+                    "id": "87887ff2-4de2-432a-a702-c0597aecece9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/15.png"
+                },
+                {
+                    "id": "059fbbc5-f34b-474e-9aa1-764104100ffd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/352.png"
+                },
+                {
+                    "id": "6c8d836f-d228-4420-9365-70c7faf178c9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/15.png"
+                }
+            ],
+            "art_caption": "television",
+            "art_description": "Popular she your small. Keep edge should different. Not way increase develop."
+        },
+        {
+            "id": "165f8c96-3cc1-47c2-ae09-503c25748561",
+            "artist_name": "produce",
+            "home_carousel": [
+                {
+                    "id": "0b48cf22-04c0-4c06-b737-ec2218117eee",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/20.png"
+                },
+                {
+                    "id": "d9eeed5b-d53b-40e5-8601-071e750d2281",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/227.png"
+                },
+                {
+                    "id": "5dc1873a-958a-4f41-ac41-cb2af5508ca3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/128.png"
+                },
+                {
+                    "id": "c6bce222-ef91-4f17-a2d1-074e532a2d93",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/98.png"
+                },
+                {
+                    "id": "32796f80-5854-4aba-8bb3-872925406b6c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/141.png"
+                }
+            ],
+            "art_caption": "turn",
+            "art_description": "Because wear southern century. Drop chance look few team event center. Name wall situation Mrs box response process phone."
+        }
+    ]
+}
